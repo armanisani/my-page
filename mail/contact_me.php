78bf8d051e23760d8cc1d1ef93642109cc223293
Arman Sani <armanisani@gmail.com>
2016-04-28 23:26:52 -0700

    added another project to portfolio, hid contact me for now, added contact info on footer, and am working on fixing some bugs

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -14,13 +11,13 @@ if(empty($_POST['name'])  		||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
+// $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'armanisani@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
